c4aa573d498f41fb910bd678311d457b2ac53555
Amit-KumarOO7 <amitkumar01122000@gmail.com>
2021-01-21 01:26:55 +0530

    Account model created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from djanog.core.urlresolvers import reverse_lazy
+from djanog.views.generic import CreateView
 
+from . import forms
 # Create your views here.
+
+class SignUp(CreateView):
+    pass
